79d0090ac87978697a5b4504c1629b79fabd093a
saadbinmunir21 <saadbinmunir21@gmail.com>
2025-10-23 14:19:02 +0500

    Reference made optional

--- models/Transaction.js
+++ models/Transaction.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const transactionSchema = new mongoose.Schema({
   account: { type: mongoose.Schema.Types.ObjectId, ref: 'Account', required: true },
   dateOfEntry: { type: Date, required: true },
-  reference: { type: Number, required: true },
+  reference: { type: Number, required: false },
   description: { type: String },
   dueOn: { type: Date },
   debit: { type: Number, default: 0 },
